67672bbdafde84835bee78b5b2805f1e6216f0e0
Tarik Mekić <tmekic1@etf.unsa.ba>
2022-05-04 00:19:14 +0200

    upravljanje postavkama i navbar

--- app/src/main/java/ba/etf/nrsprojekat/MainActivity.kt
+++ app/src/main/java/ba/etf/nrsprojekat/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = admin_user_change()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame, fragment)
+            commit()
+        }
     }
 }
