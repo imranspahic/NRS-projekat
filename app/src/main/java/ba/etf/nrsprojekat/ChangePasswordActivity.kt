f88bb3a16dcb5ad87dca6ea20c6e47e341fce95f
Imran <imranspahic300@gmail.com>
2022-05-05 10:15:29 +0200

    implementacija promjene šifre

--- app/src/main/java/ba/etf/nrsprojekat/ChangePasswordActivity.kt
+++ app/src/main/java/ba/etf/nrsprojekat/ChangePasswordActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.core.view.isVisible
+import ba.etf.nrsprojekat.services.LoginService
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
@@ -16,6 +18,54 @@ class ChangePasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
 
+        passwordField = findViewById(R.id.changePassword1Field)
+        confirmPasswordField = findViewById(R.id.changePassword2Field)
+        confirmSaveDugme = findViewById(R.id.changePasswordSaveDugme)
 
+        confirmSaveDugme.isEnabled = false
+
+        passwordField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkSaveButtonState()
+            }
+        })
+
+        confirmPasswordField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkSaveButtonState()
+            }
+        })
+
+        confirmSaveDugme.setOnClickListener {
+            onConfirm()
+        }
+
+    }
+
+    private fun checkSaveButtonState() {
+        var passwordCondition = passwordField.text?.isNotEmpty() ?: false && passwordField.text?.length!! >= 8
+        var confirmPasswordCondition =  confirmPasswordField.text?.isNotEmpty() ?: false && confirmPasswordField.text.toString() == passwordField.text.toString()
+        confirmSaveDugme.isEnabled = passwordCondition && confirmPasswordCondition
+    }
+
+    private fun onConfirm() {
+        LoginService.changePassword(passwordField.text.toString()) {
+            result ->
+            if(result) {
+                finish()
+            }
+        }
     }
 }
