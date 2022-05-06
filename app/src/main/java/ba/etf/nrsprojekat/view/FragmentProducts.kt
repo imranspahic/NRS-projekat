0c20a00f85605ccef0f2e7260196e7210f424fbe
Imran <imranspahic300@gmail.com>
2022-05-06 23:19:06 +0200

    lista proizvoda layout, broj proizvoda status i refresh dugme

--- app/src/main/java/ba/etf/nrsprojekat/view/FragmentProducts.kt
+++ app/src/main/java/ba/etf/nrsprojekat/view/FragmentProducts.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.nrsprojekat.R
 import ba.etf.nrsprojekat.services.ProductsService
+import com.google.android.material.button.MaterialButton
 
 class FragmentProducts : Fragment() {
 
+    private lateinit var brojProizvodaText: TextView
+    private lateinit var refreshDugme: MaterialButton
     private lateinit var proizvodiRecyclerView: RecyclerView
     private lateinit var productListAdapter: ProductListAdapter
 
@@ -21,12 +25,27 @@ class FragmentProducts : Fragment() {
     ): View? {
         var view = inflater.inflate(R.layout.fragment_products, container, false)
 
+        brojProizvodaText = view.findViewById(R.id.brojProizvoda)
+        refreshDugme = view.findViewById(R.id.refreshProductDugme)
         proizvodiRecyclerView = view.findViewById(R.id.proizvodiRecyclerView)
         proizvodiRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         productListAdapter = ProductListAdapter(ProductsService.products)
+        brojProizvodaText.text = ProductsService.products.size.toString()
         proizvodiRecyclerView.adapter = productListAdapter
         ProductsService.fetchProducts() {result ->
-            if(result) productListAdapter.updateProducts(ProductsService.products)
+            if(result) {
+                productListAdapter.updateProducts(ProductsService.products)
+                brojProizvodaText.text = ProductsService.products.size.toString()
+            }
+        }
+
+        refreshDugme.setOnClickListener {
+            ProductsService.fetchProducts() {result ->
+                if(result) {
+                    productListAdapter.updateProducts(ProductsService.products)
+                    brojProizvodaText.text = ProductsService.products.size.toString()
+                }
+            }
         }
         return view
     }
