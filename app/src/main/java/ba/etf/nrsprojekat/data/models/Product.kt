c28d2ec6600f7b837e19f1224f14ff8ca7544713
Imran <imranspahic300@gmail.com>
2022-05-07 12:56:58 +0200

    implementacija dodavanja proizvoda

--- app/src/main/java/ba/etf/nrsprojekat/data/models/Product.kt
+++ app/src/main/java/ba/etf/nrsprojekat/data/models/Product.kt
@@ -1,12 +1,14 @@
 package ba.etf.nrsprojekat.data.models
 
+import java.util.*
+
 data class Product(
     val id: String,
     var name: String,
-    var poslovnicaID: String,
     var poslovnicaName: String,
     var quantity: Int,
-    var status: String
+    var status: String,
+    var updatedAt: Date
 ) {
     init {
         if(poslovnicaName.isEmpty()) {
