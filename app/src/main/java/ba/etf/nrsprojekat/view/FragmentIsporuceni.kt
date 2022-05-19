644a632fe9db19e03b90831114f30b490598198f
Ajla Habib <ahabib1@etf.unsa.ba>
2022-05-19 22:40:18 +0200

    Popravljeno dodavanje proizvoda na listu uz implementaciju swipeRefreshLayout-a

--- app/src/main/java/ba/etf/nrsprojekat/view/FragmentIsporuceni.kt
+++ app/src/main/java/ba/etf/nrsprojekat/view/FragmentIsporuceni.kt
@@ -5,12 +5,14 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import ba.etf.nrsprojekat.R
 import ba.etf.nrsprojekat.services.ProductsService
 
 class FragmentIsporuceni : Fragment(R.layout.isporuceni_proizvodi_fragment) {
     private lateinit var recyclerViewDelivered: RecyclerView
     private lateinit var deliveredAdapter: DeliveredAdapter
+    private lateinit var swipeRefreshLayout2: SwipeRefreshLayout
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -22,6 +24,15 @@ class FragmentIsporuceni : Fragment(R.layout.isporuceni_proizvodi_fragment) {
         ProductsService.getDeliveredProducts { result ->
             val myAdapter = DeliveredAdapter(result)
             recyclerViewDelivered.adapter = myAdapter
+        }
+        swipeRefreshLayout2 = view.findViewById(R.id.swipeRefreshLayout2)
+        swipeRefreshLayout2.setOnRefreshListener {
+            ProductsService.getDeliveredProducts { result ->
+                val myAdapter = DeliveredAdapter(result)
+                recyclerViewDelivered.adapter = myAdapter
+            }
+            recyclerViewDelivered.adapter!!.notifyDataSetChanged()
+            swipeRefreshLayout2.isRefreshing = false
         }
     }
     override fun onResume() {
