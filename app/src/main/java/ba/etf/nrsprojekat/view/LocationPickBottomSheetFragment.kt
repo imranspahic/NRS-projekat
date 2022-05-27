2206a138ec6cda1e711e1504d304d37e0c667d96
Tarik Mekić <tmekic1@etf.unsa.ba>
2022-05-27 11:32:25 +0200

    branchevi

--- app/src/main/java/ba/etf/nrsprojekat/view/LocationPickBottomSheetFragment.kt
+++ app/src/main/java/ba/etf/nrsprojekat/view/LocationPickBottomSheetFragment.kt
@@ -7,15 +7,18 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import ba.etf.nrsprojekat.R
 import ba.etf.nrsprojekat.services.BranchesService
+import ba.etf.nrsprojekat.services.LoginService
 import ba.etf.nrsprojekat.services.OrderServices
+import ba.etf.nrsprojekat.services.UserService
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firestore.v1.StructuredQuery
 
 class LocationPickBottomSheetFragment()  : BottomSheetDialogFragment() {
-    private lateinit var nazivPoslovniceSpiner : Spinner
-    private lateinit var nazivLokacijeSpiner : Spinner
+    private lateinit var nazivPoslovnice : TextView
+    private lateinit var mjestaPoslovniceSpiner : Spinner
     private lateinit var dodajLokacijuDugme : Button
     private lateinit var zatvoriLokacijuDugme : Button
 
@@ -25,8 +28,9 @@ class LocationPickBottomSheetFragment()  : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.location_pick_bottom_sheet, container, false)
-        nazivPoslovniceSpiner = view.findViewById(R.id.nazivPoslovniceSpiner)
-        nazivLokacijeSpiner = view.findViewById(R.id.nazivLokacijeSpiner)
+
+        nazivPoslovnice = view.findViewById(R.id.nazivPoslovniceText)
+        mjestaPoslovniceSpiner = view.findViewById(R.id.mjestaPoslovniceSpiner)
         dodajLokacijuDugme = view.findViewById(R.id.DodajLokacijuDugme)
         zatvoriLokacijuDugme = view.findViewById(R.id.ZatvoriLokacijuDugme)
         return view
@@ -34,12 +38,27 @@ class LocationPickBottomSheetFragment()  : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        nazivPoslovnice.text = LoginService.logovaniKorisnik?.poslovnica()
+
+        var mjestaZaPoslovnicu = mutableListOf<String>()
+
+        BranchesService.getMjesta(nazivPoslovnice.text.toString()) {
+            for(item in it.mjesto) mjestaZaPoslovnicu.add(item)
+            val adapterZaSpiner = ArrayAdapter(requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                mjestaZaPoslovnicu
+            )
+            mjestaPoslovniceSpiner.adapter = adapterZaSpiner
+        }
+
+
+
         zatvoriLokacijuDugme.setOnClickListener {
             dismiss()
         }
         dodajLokacijuDugme.setOnClickListener {
-            OrderServices.lokacija = nazivPoslovniceSpiner.selectedItem.toString()
-            OrderServices.mjesto = nazivLokacijeSpiner.selectedItem.toString()
+            OrderServices.lokacija = nazivPoslovnice.text.toString()
+            OrderServices.mjesto = mjestaPoslovniceSpiner.selectedItem.toString()
             dismiss()
         }
 
